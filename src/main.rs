9aeade7db207ac97cf882e04e5aa7a54e5b37932
Dan Munckton <munckfish@gmail.com>
2019-11-25 13:42:30 +0000

    Extract read_array

--- src/main.rs
+++ src/main.rs
@@ -29,52 +29,68 @@ impl fmt::Display for Header {
 }
 
 fn handle_client(mut stream: TcpStream) -> Result<()> {
-    let mut reader = BufReader::new(&stream);
+    let reader = BufReader::new(&stream);
+
+    let request_vec = match read_array(reader) {
+        Ok(value) => value,
+        Err(msg) => {
+            eprintln!("{}", msg);
+            stream.write_all(b"-Parser error\r\n")?;
+            return Ok(());
+        }
+    };
+
+    for pair in request_vec {
+        println!("  \"{:?}\"", pair);
+    }
+
+    stream.write_all(b"+OK\r\n")?;
+
+    println!("Closing connection.");
+    Ok(())
+}
+
+fn read_array(mut reader: impl BufRead) -> Result<Vec<(Header, String)>> {
     let mut request_header_buf = vec![];
 
     if let Err(msg) = read_line(&mut reader, &mut request_header_buf) {
-        eprintln!("{} while parsing line", msg);
-        stream.write_all(b"-Parser error\r\n")?;
-        return Ok(());
+        let err_msg = format!("{} while parsing line", msg);
+        return Err(err_msg.into());
     }
 
     let request_header = match parse_header(&request_header_buf) {
         Ok(header) => header,
         Err(msg) => {
-            eprintln!("{} from header str {:?}", msg, request_header_buf);
-            stream.write_all(b"-Parser error\r\n")?;
-            return Ok(());
+            let err_msg = format!("{} from header str {:?}", msg, request_header_buf);
+            return Err(err_msg.into());
         }
     };
 
     println!("    {}", request_header);
 
     if request_header.type_sym != b'*' {
-        eprintln!("request is not an array");
-        stream.write_all(b"-Parser error\r\n")?;
-        return Ok(());
+        let err_msg = format!("request is not an array");
+        return Err(err_msg.into());
     }
 
-    let mut req_array: Vec<(Header, String)> = Vec::new();
+    let mut req_array = Vec::new();
 
     for _ in 0..request_header.len {
         let mut elem_header_buf: Vec<u8> = Vec::new();
 
         if let Err(msg) = read_line(&mut reader, &mut elem_header_buf) {
-            eprintln!("{} while reading elem header line", msg);
-            stream.write_all(b"-Parser error\r\n")?;
-            return Ok(());
+            let err_msg = format!("{} while reading elem header line", msg);
+            return Err(err_msg.into());
         }
 
         let elem_resp_header = match parse_header(&elem_header_buf) {
             Ok(header) => header,
             Err(msg) => {
-                eprintln!(
+                let err_msg = format!(
                     "{} while reading elem header string {:?}",
                     msg, elem_header_buf
                 );
-                stream.write_all(b"-Parser error\r\n")?;
-                return Ok(());
+                return Err(err_msg.into());
             }
         };
         println!("    {}", elem_resp_header);
@@ -82,9 +98,8 @@ fn handle_client(mut stream: TcpStream) -> Result<()> {
         let elem_value_str = match read_bulk_string(&mut reader, &elem_resp_header) {
             Ok(value) => value,
             Err(msg) => {
-                eprintln!("{} while reading elem value string", msg);
-                stream.write_all(b"-Parser error\r\n")?;
-                return Ok(());
+                let err_msg = format!("{} while reading elem value string", msg);
+                return Err(err_msg.into());
             }
         };
         println!("    value({:?})", elem_value_str);
@@ -92,15 +107,7 @@ fn handle_client(mut stream: TcpStream) -> Result<()> {
         req_array.push((elem_resp_header, elem_value_str));
     }
 
-    println!("    \"{:?}\"", request_header);
-    for pair in req_array {
-        println!("      \"{:?}\"", pair);
-    }
-
-    stream.write_all(b"+OK\r\n")?;
-
-    println!("Closing connection.");
-    Ok(())
+    Ok(req_array)
 }
 
 fn read_line(reader: &mut impl BufRead, buffer: &mut Vec<u8>) -> Result<()> {
