2263de3e8f51b8ee707fa31c84e90a556bbb87de
Dan Munckton <munckfish@gmail.com>
2019-11-25 17:44:42 +0000

    Keep connection open between requests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fmt;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::error;
 
-type BoxedError = Box<dyn std::error::Error>;
+type BoxedError = Box<dyn error::Error>;
 type Result<T> = std::result::Result<T, BoxedError>;
 
 fn main() -> Result<()> {
@@ -28,32 +29,39 @@ impl fmt::Display for Header {
     }
 }
 
-fn handle_client(mut stream: TcpStream) -> Result<()> {
-    let reader = BufReader::new(&stream);
+fn handle_client(stream: TcpStream) -> Result<()> {
+    let mut out_stream = stream.try_clone()?;
+    let mut reader = BufReader::new(&stream);
 
-    let request_vec = match read_array(reader) {
-        Ok(value) => value,
-        Err(msg) => {
-            eprintln!("{}", msg);
-            stream.write_all(b"-Parser error\r\n")?;
-            return Ok(());
+    loop {
+        let request_vec = match read_array(&mut reader) {
+            Ok(value) => value,
+            Err(err) => {
+                let msg = err.to_string();
+                // HACK replace with proper custom error type
+                if !msg.starts_with("empty") {
+                    eprintln!("{}", msg);
+                    out_stream.write_all(b"-Parser error\r\n")?;
+                }
+                break;
+            },
+        };
+
+        for pair in request_vec {
+            println!("  \"{:?}\"", pair);
         }
-    };
 
-    for pair in request_vec {
-        println!("  \"{:?}\"", pair);
+        out_stream.write_all(b"+OK\r\n")?;
     }
-
-    stream.write_all(b"+OK\r\n")?;
 
     println!("Closing connection.");
     Ok(())
 }
 
-fn read_array(mut reader: impl BufRead) -> Result<Vec<(Header, String)>> {
+fn read_array(reader: &mut impl BufRead) -> Result<Vec<(Header, String)>> {
     let mut request_header_buf = vec![];
 
-    if let Err(msg) = read_line(&mut reader, &mut request_header_buf) {
+    if let Err(msg) = read_line(reader, &mut request_header_buf) {
         let err_msg = format!("{} while parsing line", msg);
         return Err(err_msg.into());
     }
@@ -78,7 +86,7 @@ fn read_array(mut reader: impl BufRead) -> Result<Vec<(Header, String)>> {
     for _ in 0..request_header.len {
         let mut elem_header_buf: Vec<u8> = Vec::new();
 
-        if let Err(msg) = read_line(&mut reader, &mut elem_header_buf) {
+        if let Err(msg) = read_line(reader, &mut elem_header_buf) {
             let err_msg = format!("{} while reading elem header line", msg);
             return Err(err_msg.into());
         }
@@ -95,7 +103,7 @@ fn read_array(mut reader: impl BufRead) -> Result<Vec<(Header, String)>> {
         };
         println!("    {}", elem_resp_header);
 
-        let elem_value_str = match read_bulk_string(&mut reader, &elem_resp_header) {
+        let elem_value_str = match read_bulk_string(reader, &elem_resp_header) {
             Ok(value) => value,
             Err(msg) => {
                 let err_msg = format!("{} while reading elem value string", msg);
@@ -112,7 +120,10 @@ fn read_array(mut reader: impl BufRead) -> Result<Vec<(Header, String)>> {
 
 fn read_line(reader: &mut impl BufRead, buffer: &mut Vec<u8>) -> Result<()> {
     let num_bytes = reader.read_until(b'\n', buffer)?;
-    if num_bytes < 2 {
+
+    if num_bytes == 0 {
+        return Err("empty".into());
+    } else if num_bytes < 2 {
         return Err("line too short".into());
     }
 
