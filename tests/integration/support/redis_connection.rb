aef956e0617085a8994392cc5d352738768f6458
Dan Munckton <munckfish@gmail.com>
2019-12-30 14:38:39 +0000

    Make it easy to switch between testing the clone and real Redis

--- tests/integration/support/redis_connection.rb
+++ tests/integration/support/redis_connection.rb
@@ -1,5 +1,12 @@
 RSpec.shared_context "Redis connection" do
-  let(:redis) { Redis.new(port: 8080) }
+  let(:port) do
+    if ENV.key? 'TEST_REAL_REDIS'
+      6379
+    else
+      8080
+    end
+  end
+  let(:redis) { Redis.new(port: port) }
 
   around(:example) do |example|
     example.run
@@ -10,4 +17,13 @@ end
 
 RSpec.configure do |rspec|
   rspec.include_context "Redis connection", :include_connection => true
+
+  rspec.after(:suite) do
+    if ENV.key? 'TEST_REAL_REDIS'
+      puts
+      puts "XXXX -------------------------------------------------------------"
+      puts "XXXX tests were ran against the real Redis"
+      puts "XXXX -------------------------------------------------------------"
+    end
+  end
 end
