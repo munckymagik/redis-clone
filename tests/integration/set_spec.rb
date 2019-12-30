78495ae3627585b96e51b5494a292b57b9ec93ed
Dan Munckton <munckfish@gmail.com>
2019-12-30 13:31:13 +0000

    Flush the db after every test

--- tests/integration/set_spec.rb
+++ tests/integration/set_spec.rb
@@ -1,9 +1,7 @@
 RSpec.describe "SET", include_connection: true do
   it "sets a key and value" do
-    reply = redis.set("x", "123")
-    expect(reply).to eql("OK")
-
-    value = redis.get("x")
-    expect(value).to eql("123")
+    expect(redis.get("x")).to be_nil
+    expect(redis.set("x", "123")).to eql("OK")
+    expect(redis.get("x")).to eql("123")
   end
 end
--- tests/integration/support/redis_connection.rb
+++ tests/integration/support/redis_connection.rb
@@ -1,5 +1,11 @@
 RSpec.shared_context "Redis connection" do
   let(:redis) { Redis.new(port: 8080) }
+
+  around(:example) do |example|
+    example.run
+  ensure
+    redis.flushdb
+  end
 end
 
 RSpec.configure do |rspec|
