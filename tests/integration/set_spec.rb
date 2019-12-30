dae70c1fb5861751a93fa73d29a952933cc31cc1
Dan Munckton <munckfish@gmail.com>
2019-12-30 13:35:15 +0000

    Expand set_spec

--- tests/integration/set_spec.rb
+++ tests/integration/set_spec.rb
@@ -1,7 +1,19 @@
 RSpec.describe "SET", include_connection: true do
-  it "sets a key and value" do
-    expect(redis.get("x")).to be_nil
-    expect(redis.set("x", "123")).to eql("OK")
-    expect(redis.get("x")).to eql("123")
+  context "when the key does not exist" do
+    it "sets the key to hold the string value" do
+      expect(redis.get("x")).to be_nil
+      expect(redis.set("x", "123")).to eql("OK")
+      expect(redis.get("x")).to eql("123")
+    end
+  end
+
+  context "when key already holds a value" do
+    it "it is overwritten" do
+      redis.set("x", "123")
+      expect(redis.get("x")).to eql("123")
+
+      expect(redis.set("x", "456")).to eql("OK")
+      expect(redis.get("x")).to eql("456")
+    end
   end
 end
