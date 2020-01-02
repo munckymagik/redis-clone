9e41c2640ee88a15f1ee6186fcc937dc1ae527d8
Dan Munckton <munckfish@gmail.com>
2020-01-02 19:52:35 +0000

    Fully implement DEL

--- src/commands/del.rs
+++ src/commands/del.rs
@@ -1,12 +1,15 @@
 use crate::{db::Database, errors::Result, request::Request, response::Response};
 
 pub(crate) fn call(db: &mut Database, request: &Request, response: &mut Response) -> Result<()> {
-    let key = request.arg(0).unwrap();
+    let mut count = 0;
 
-    match db.remove(key) {
-        Some(_) => response.add_integer(1),
-        None => response.add_integer(0),
+    for key in request.arguments() {
+        if let Some(_) = db.remove(key) {
+            count += 1;
+        }
     }
+
+    response.add_integer(count);
 
     Ok(())
 }
--- tests/integration/del_spec.rb
+++ tests/integration/del_spec.rb
@@ -7,12 +7,27 @@ RSpec.describe "DEL", include_connection: true do
     )
   end
 
-  it "removes the specified keys" do
-    redis.set("x", "123")
-    expect(redis.get("x")).to eql("123")
+  context "when the specified key exists" do
+    it "removes the key and returns 1" do
+      redis.set("x", "123")
+      expect(redis.del("x")).to eql(1)
+      expect(redis.exists("x")).to be(false)
+    end
+  end
 
-    redis.del("x")
+  context "when the specified key does not exist" do
+    it "removes the key and returns 0" do
+      expect(redis.del("does-not-exist")).to eql(0)
+      expect(redis.exists("does-not-exist")).to be(false)
+    end
+  end
 
-    expect(redis.get("x")).to be_nil
+  context "when there are multiple keys to remove" do
+    it "removes the keys and returns the count of removed" do
+      redis.set("x", "123")
+      redis.set("y", "123")
+      expect(redis.del("x", "y", "does-not-exist")).to eql(2)
+      expect(redis.call("exists", "x", "y")).to eql(0)
+    end
   end
 end
