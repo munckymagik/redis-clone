553839cfc5deba5f4bf88ed5da7db75b21018cdd
Dan Munckton <munckfish@gmail.com>
2020-01-02 20:57:29 +0000

    Implement NX and XX arguments for SET

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -3,6 +3,27 @@ use crate::{db::Database, errors::Result, request::Request, response::Response};
 pub(crate) fn call(db: &mut Database, request: &Request, response: &mut Response) -> Result<()> {
     let key = request.arg(0).unwrap();
     let value = request.arg(1).unwrap();
+    let mut nx = false;
+    let mut xx = false;
+
+    for arg in &request.arguments()[2..] {
+        match arg.to_lowercase().as_ref() {
+            "nx" if !xx => nx = true,
+            "xx" if !nx => xx = true,
+            _ => {
+                response.add_error("ERR syntax error");
+                return Ok(());
+            },
+        }
+    }
+
+    if nx && db.contains_key(key)
+        || xx && !db.contains_key(key)
+    {
+        response.add_null_string();
+        return Ok(());
+    }
+
     db.insert(key.to_owned(), value.to_owned());
     response.add_simple_string("OK");
 
--- tests/integration/set_spec.rb
+++ tests/integration/set_spec.rb
@@ -13,19 +13,46 @@ RSpec.describe "SET", include_connection: true do
 
   context "when the key does not exist" do
     it "sets the key to hold the string value" do
-      expect(redis.get("x")).to be_nil
       expect(redis.set("x", "123")).to eql("OK")
       expect(redis.get("x")).to eql("123")
+    end
+
+    it "sets the key to hold the string value if NX is specified" do
+      expect(redis.set("x", "123", nx: true)).to be(true)
+      expect(redis.get("x")).to eql("123")
+    end
+
+    it "does not set the key if XX is specified" do
+      expect(redis.set("x", "123", xx: true)).to be(false)
+      expect(redis.exists("x")).to be(false)
     end
   end
 
   context "when key already holds a value" do
-    it "it is overwritten" do
+    before do
       redis.set("x", "123")
-      expect(redis.get("x")).to eql("123")
+    end
 
+    it "is overwritten" do
       expect(redis.set("x", "456")).to eql("OK")
       expect(redis.get("x")).to eql("456")
+    end
+
+    it "is overwritten if XX is specified" do
+      expect(redis.set("x", "456", xx: true)).to be(true)
+      expect(redis.get("x")).to eql("456")
+    end
+
+    it "is not overwritten if NX is specified" do
+      expect(redis.set("x", "456", nx: true)).to be(false)
+      expect(redis.get("x")).to eql("123")
+    end
+  end
+
+  context "if both nx and xx are specified" do
+    it 'responds with a syntax error' do
+      expect { redis.set("x", "y", xx: true, nx: true) }
+        .to raise_error("ERR syntax error")
     end
   end
 end
