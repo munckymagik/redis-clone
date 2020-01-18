653c4dc237f8c8b5fd1b6dd5909181714a98bff0
Dan Munckton <munckfish@gmail.com>
2020-01-18 20:25:30 +0000

    Update the benchmark script

---
+++ benches/clone-vs-real.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+benches="set,get,incr,lpush,rpush,lpop,rpop,lrange"
+opts="-q"
+
+redis-cli flushdb
+redis-cli -p 8080 flushdb
+
+echo "Real Redis"
+echo "------------------------------------------------------------------------"
+redis-benchmark $opts -t $benches
+
+echo "Clone Redis"
+echo "------------------------------------------------------------------------"
+redis-benchmark $opts -t $benches -p 8080
