4413ca4d6a3f8831103ae107848f7508f55a878f
Dan Munckton <munckfish@gmail.com>
2020-03-04 20:21:24 +0000

    Switch to ByteString in list_type

--- src/commands/list_type.rs
+++ src/commands/list_type.rs
@@ -10,7 +10,7 @@ use std::convert::TryInto;
 
 macro_rules! parse_arg_or_reply_with_err {
     ($idx:literal, $req:expr, $resp:expr) => {
-        match $req.arg($idx)?.parse() {
+        match $req.bs_arg($idx)?.parse() {
             Ok(n) => n,
             Err(_) => {
                 $resp.add_reply_not_a_number();
