def2f7d0af7184c35f852d47c03297a4fe5f7768
Dan Munckton <munckfish@gmail.com>
2020-03-07 19:43:40 +0000

    byte_string: refactor from_bytes

--- byte_string/src/from_bytes.rs
+++ byte_string/src/from_bytes.rs
@@ -19,33 +19,27 @@ pub trait Number: Sized + Neg<Output = Self> + Copy {
     fn checked_mul(self, rhs: Self) -> Option<Self>;
 }
 
-impl Number for i64 {
-    fn from(n: u32) -> Self {
-        n as Self
-    }
+macro_rules! impl_number_for {
+    ($($t:ty),+) => {
+        $(
+            impl Number for $t {
+                fn from(n: u32) -> Self {
+                    n as Self
+                }
 
-    fn checked_add(self, rhs: Self) -> Option<Self> {
-        self.checked_add(rhs)
-    }
+                fn checked_add(self, rhs: Self) -> Option<Self> {
+                    self.checked_add(rhs)
+                }
 
-    fn checked_mul(self, rhs: Self) -> Option<Self> {
-        self.checked_mul(rhs)
+                fn checked_mul(self, rhs: Self) -> Option<Self> {
+                    self.checked_mul(rhs)
+                }
+            }
+        )+
     }
 }
 
-impl Number for isize {
-    fn from(n: u32) -> Self {
-        n as Self
-    }
-
-    fn checked_add(self, rhs: Self) -> Option<Self> {
-        self.checked_add(rhs)
-    }
-
-    fn checked_mul(self, rhs: Self) -> Option<Self> {
-        self.checked_mul(rhs)
-    }
-}
+impl_number_for!(i64, isize);
 
 // Based heavily on Rust std lib's from_str_radix
 pub(crate) fn from_bytes<T: Number>(string: &[u8]) -> Result<T, ParseIntError> {
@@ -75,56 +69,40 @@ pub(crate) fn from_bytes<T: Number>(string: &[u8]) -> Result<T, ParseIntError> {
 mod tests {
     use super::*;
 
+    macro_rules! test_for_type {
+        ($t:ty, $i:ident) => {
+            // Success cases
+            assert_eq!(from_bytes::<$t>(b"0"), Ok(0));
+            assert_eq!(from_bytes::<$t>(b"1"), Ok(1));
+            assert_eq!(from_bytes::<$t>(b"+0"), Ok(0));
+            assert_eq!(from_bytes::<$t>(b"-0"), Ok(0));
+            assert_eq!(from_bytes::<$t>(b"+1"), Ok(1));
+            assert_eq!(from_bytes::<$t>(b"-1"), Ok(-1));
+            assert_eq!(from_bytes::<$t>(b"+9223372036854775807"), Ok(std::$i::MAX));
+            assert_eq!(from_bytes::<$t>(b"-9223372036854775808"), Ok(std::$i::MIN));
+
+            // Error cases
+            assert_eq!(from_bytes::<$t>(b""), Err(ParseIntError));
+            assert_eq!(from_bytes::<$t>(b"x"), Err(ParseIntError));
+
+            // The final mul by the radix will overflow
+            assert_eq!(from_bytes::<$t>(b"92233720368547758071"), Err(ParseIntError));
+
+            // The adding the final 8 digit will overflow
+            assert_eq!(from_bytes::<$t>(b"9223372036854775808"), Err(ParseIntError));
+
+            fn expect_std_error(_: impl std::error::Error) {}
+            expect_std_error(ParseIntError);
+        };
+    }
+
     #[test]
     fn test_from_bytes_i64() {
-        // Success cases
-        assert_eq!(from_bytes::<i64>(b"0"), Ok(0));
-        assert_eq!(from_bytes::<i64>(b"1"), Ok(1));
-        assert_eq!(from_bytes::<i64>(b"+0"), Ok(0));
-        assert_eq!(from_bytes::<i64>(b"-0"), Ok(0));
-        assert_eq!(from_bytes::<i64>(b"+1"), Ok(1));
-        assert_eq!(from_bytes::<i64>(b"-1"), Ok(-1));
-        assert_eq!(from_bytes::<i64>(b"+9223372036854775807"), Ok(std::i64::MAX));
-        assert_eq!(from_bytes::<i64>(b"-9223372036854775808"), Ok(std::i64::MIN));
-
-        // Error cases
-        assert_eq!(from_bytes::<i64>(b""), Err(ParseIntError));
-        assert_eq!(from_bytes::<i64>(b"x"), Err(ParseIntError));
-
-        // The final mul by the radix will overflow
-        assert_eq!(from_bytes::<i64>(b"92233720368547758071"), Err(ParseIntError));
-
-        // The adding the final 8 digit will overflow
-        assert_eq!(from_bytes::<i64>(b"9223372036854775808"), Err(ParseIntError));
-
-        fn expect_std_error(_: impl std::error::Error) {}
-        expect_std_error(ParseIntError);
+        test_for_type!(i64, i64);
     }
 
     #[test]
     fn test_from_bytes_isize() {
-        // Success cases
-        assert_eq!(from_bytes::<isize>(b"0"), Ok(0));
-        assert_eq!(from_bytes::<isize>(b"1"), Ok(1));
-        assert_eq!(from_bytes::<isize>(b"+0"), Ok(0));
-        assert_eq!(from_bytes::<isize>(b"-0"), Ok(0));
-        assert_eq!(from_bytes::<isize>(b"+1"), Ok(1));
-        assert_eq!(from_bytes::<isize>(b"-1"), Ok(-1));
-        assert_eq!(from_bytes::<isize>(b"+9223372036854775807"), Ok(std::isize::MAX));
-        assert_eq!(from_bytes::<isize>(b"-9223372036854775808"), Ok(std::isize::MIN));
-
-        // Error cases
-        assert_eq!(from_bytes::<isize>(b""), Err(ParseIntError));
-        assert_eq!(from_bytes::<isize>(b"x"), Err(ParseIntError));
-
-        // The final mul by the radix will overflow
-        assert_eq!(from_bytes::<isize>(b"92233720368547758071"), Err(ParseIntError));
-
-        // The adding the final 8 digit will overflow
-        assert_eq!(from_bytes::<isize>(b"9223372036854775808"), Err(ParseIntError));
-
-        fn expect_std_error(_: impl std::error::Error) {}
-        expect_std_error(ParseIntError);
+        test_for_type!(isize, isize);
     }
-
 }
